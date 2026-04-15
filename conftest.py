edc29d65660594d0a8024b666e7636d7475593b4
lmesliuk <luciana_mesliuk@yahoo.com.ar>
2026-04-15 00:11:26 -0300

    Fix screeshots issue

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
 import pytest 
-'''
-@pytest.fixture
-def base_url():
-    return "https://opensource-demo.orangehrmlive.com"
-'''
+
+@pytest.fixture(autouse=True)
+def screenshot_on_failure(page, request):
+    yield
+    if request.node.rep_call.failed:
+        page.screenshot(path=f"screenshots/{request.node.name}.png")
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
--- tests/test_login_invalid.py
+++ tests/test_login_invalid.py
@@ -1,9 +1,12 @@
 from pages.login_page import LoginPage
+from playwright.sync_api import expect
 
-def test_successful_login(page):
+def test_invalid_login(page):
     login = LoginPage(page)
     login.open()
     login.login("Admin", "pepe123")
+    expect(page.get_by_text("Invalid credentials")).to_be_visible()
+    #page.screenshot(path="shot.png", full_page=True)
     assert login.get_error_message() == "Invalid credentials"
     
     
