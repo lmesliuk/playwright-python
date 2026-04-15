8109215bf33239cc629b022e1e9b7a5878ba94cb
lmesliuk <luciana_mesliuk@yahoo.com.ar>
2026-04-15 00:31:50 -0300

    Fix logging issues

--- conftest.py
+++ conftest.py
@@ -1,4 +1,22 @@
+import logging
 import pytest 
+import os
+
+os.makedirs("screenshots", exist_ok=True)
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] %(name)s - %(message)s"
+)
+
+file_handler = logging.FileHandler("logs/test.log", mode="w", encoding="utf-8")
+file_handler.setFormatter(formatter)
+
+if not logger.handlers:
+    logger.addHandler(file_handler)
 
 @pytest.fixture(autouse=True)
 def screenshot_on_failure(page, request):
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,7 @@
 from playwright.sync_api import expect
+import logging
+
+logger = logging.getLogger(__name__) # Set up a logger for this module
 
 class LoginPage:
     URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
@@ -7,23 +10,21 @@ class LoginPage:
         self.page = page
 
     def open(self):
+        logger.info("Opening login")
         self.page.goto(self.URL)
 
     def login(self, username, password):
+        logger.info(f"Logging in with username: {username}")
         self.page.get_by_placeholder("Username").fill(username)
         self.page.get_by_placeholder("Password").fill(password)
         self.page.get_by_role("button", name="Login").click()
 
-    #def is_dashboard_displayed(self):
-    #    return self.page.get_by_role("heading", name="Dashboard").is_visible()
-
-    #def is_dashboard_displayed(self):
-     #   self.page.get_by_role("heading", name="Dashboard").wait_for()
-    #    return self.page.get_by_role("heading", name="Dashboard").is_visible()
-
     def is_dashboard_displayed(self):
+        logger.info("Checking if dashboard is displayed")
         expect(self.page.get_by_role("heading", name="Dashboard")).to_be_visible()
         return True
 
     def get_error_message(self):
-        return self.page.locator(".oxd-alert-content-text").inner_text()
+        msg = self.page.locator(".oxd-alert-content-text").inner_text()
+        logger.info(f"Error message displayed: {msg}")
+        return msg
